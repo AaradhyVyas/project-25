1300a28ea26231ad558b3de3e612bb00f7d1d1b1
AaradhyVyas <77629845+AaradhyVyas@users.noreply.github.com>
2021-05-08 12:53:49 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -1,6 +1,7 @@
 class Paper{
 	constructor(x,y,r)
 	{
+	this.image = loadImage("paper.png");
 	 var options = {
         'restitution':0.3,
         'friction':0,
@@ -11,9 +12,8 @@ class Paper{
 		this.y=y;
 		this.r=r
 		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
-        this.image = loadImage("paper.png");
 		World.add(world, this.body);
-
+	
 	}
 	display()
 	{
@@ -22,7 +22,8 @@ class Paper{
 			translate(paperpos.x, paperpos.y);
 			rectMode(CENTER)
 			imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
+           image(this.image, 0, 0, this.width, this.height);
+		   this.image.scale = 0.02
             ellipse(0,0,this.r,this.r);
 			pop()
 	}
